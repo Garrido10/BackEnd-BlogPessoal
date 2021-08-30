90ba22d87474772be260d718cd5aa595365c3667
Garrido10 <fernando.garrido98@gmail.com>
2021-08-30 05:53:03 -0300

    Back-end blog pessoal

---
+++ blogpessoal/src/main/java/br/org/generation/blogpessoal/model/UsuarioLogin.java
@@ -0,0 +1,78 @@
+package br.org.generation.blogpessoal.model;
+
+public class UsuarioLogin {
+	
+	private long id;
+
+	private String nome;
+	
+	private String usuario;
+	
+	private String senha;
+	
+	private String token;
+	
+	private String tipo;
+	
+	private String foto;
+
+
+	public long getId() {
+		return this.id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return this.nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getUsuario() {
+		return this.usuario;
+	}
+
+	public void setUsuario(String usuario) {
+		this.usuario = usuario;
+	}
+
+	public String getSenha() {
+		return this.senha;
+	}
+
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+
+	public String getToken() {
+		return this.token;
+	}
+
+	public void setToken(String token) {
+		this.token = token;
+	}
+
+	public String getTipo() {
+		return tipo;
+	}
+
+	public void setTipo(String tipo) {
+		this.tipo = tipo;
+	}
+
+	public String getFoto() {
+		return foto;
+	}
+
+	public void setFoto(String foto) {
+		this.foto = foto;
+	}
+	
+	
+
+}
